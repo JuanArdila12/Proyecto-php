52112912a377fe0b31c2336acc4911f52a78a502
Juan <jpardila38@misena.edu.co>
2020-11-07 16:03:42 -0500

    Home index

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+var menu = document.querySelector('.hamburger');
+
+
+function toggleMenu (event) {
+  this.classList.toggle('is-active');
+  document.querySelector( ".menuppal" ).classList.toggle("is_active");
+  event.preventDefault();
+}
+
+// event
+menu.addEventListener('click', toggleMenu, false);
